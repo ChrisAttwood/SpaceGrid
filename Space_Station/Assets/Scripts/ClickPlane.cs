d0908c0b1737ac1c32c2ad2979586c02b92600a7
ChrisAttwood <christopher.attwood@outlook.com>
2018-07-13 20:35:16 +0100

    Stuff

---
+++ Space_Station/Assets/Scripts/ClickPlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPlane : MonoBehaviour {
+
+    LayerMask mask;
+
+    // Use this for initialization
+    void Start () {
+        mask = LayerMask.NameToLayer("ClickPlane");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckClick();
+
+    }
+
+
+    void CheckClick()
+    {
+        RaycastHit hit;
+       //Ray ray = Camera..ScreenPointToRay(Input.mousePosition);
+
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, mask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+    }
+}
