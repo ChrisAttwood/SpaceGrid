4875daa107977e0c0d44f6239878f08f3dea7539
SebCharman <cheesemonkstylee@hotmail.com>
2018-07-17 20:06:08 +0100

    Orbit script

---
+++ Space_Station/Assets/Scripts/Orbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+    public GameObject satelliteOf; // The object which this will orbit around
+    public float orbitSpeed;
+
+    void Update () {
+        transform.RotateAround(satelliteOf.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+	}
+}
